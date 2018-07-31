1be2b108408a31579d6c79a4bf586434f09df384
Kyleigh <kyleighizatt@gmail.com>
2018-07-31 11:52:55 -0600

    added some more things

--- src/components/app.js
+++ src/components/app.js
@@ -4,10 +4,13 @@ export default class App extends Component {
   render() {
     return (
       <div class="grid">
-      <div>hi</div>
-      <div>hi</div>
-      <div>hi</div>
+      <div class= "grid__skew-dark-one-box"></div>
+      <div class= "grid__skew-dark-two"></div>
+     <div class= "grid__skew-dark-three"></div>
+
       <div class="grid__skew-light-one"></div>
+      <div class="grid__skew-light-two"></div>
+      <div class="grid__skew-light-three-box"></div>
       </div>
     );
   }
